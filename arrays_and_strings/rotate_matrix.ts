6f41d1c3a114a57d62e1d5eb7d0e620bab685d5c
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-07 21:58:40 -0300

    wip

---
+++ arrays_and_strings/rotate_matrix.ts
@@ -0,0 +1,11 @@
+import assert from "node:assert";
+import test from "node:test";
+
+/**
+ * Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+ * bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+ */
+
+test("rotate matrix", () => {
+  assert(true);
+})
