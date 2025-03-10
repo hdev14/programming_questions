99b9d9a4b0548f4f983b5beb18803d418f726418
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-10 15:45:33 -0300

    remove duplicates

---
+++ linked_list/create_linked_list.ts
@@ -0,0 +1,31 @@
+export class LinkedListNode<T = any> {
+  next?: LinkedListNode;
+  constructor(readonly value: T) { }
+}
+
+
+export function linkedListToArray(node: LinkedListNode) {
+  const array: Array<any> = [];
+  let next: LinkedListNode | undefined = node;
+  while (next) {
+    array.push(next.value);
+    next = next.next;
+  }
+  return array;
+}
+
+export function createLinkedList<T>(values: Array<T>): LinkedListNode<T> {
+  const root = new LinkedListNode(values[0]);
+
+  let previous: LinkedListNode | undefined = root;
+  for (let idx = 1; idx < values.length; idx++) {
+    const new_node = new LinkedListNode(values[idx]);
+    if (previous) {
+      previous.next = new_node;
+    }
+    previous = new_node;
+  }
+
+  return root;
+}
+
---
+++ linked_list/remove_duplicates.ts
@@ -0,0 +1,30 @@
+import assert from "node:assert";
+import test from "node:test";
+import { createLinkedList, LinkedListNode, linkedListToArray } from "./create_linked_list";
+
+/**
+ * Remove Dups: Write code to remove duplicates from an unsorted linked list.
+ * How would you solve this problem if a temporary buffer is not allowed?
+ */
+function removeDuplicates(node: LinkedListNode) {
+  const set = new Set<any>();
+
+  for (let next: any = node, previous: any = null; next !== undefined; next = next.next) {
+    if (set.has(next.value) && previous !== null) {
+      previous.next = next.next;
+      continue;
+    }
+
+    set.add(next.value);
+    previous = next;
+  }
+}
+
+test("remove duplicates", () => {
+  const linked_list = createLinkedList<number>([1, 2, 4, 5, 6, 7, 2, 1]);
+
+  removeDuplicates(linked_list);
+
+  assert.deepEqual(linkedListToArray(linked_list), [1, 2, 4, 5, 6, 7]);
+});
+
