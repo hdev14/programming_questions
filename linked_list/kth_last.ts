eededed6af69026cd23f5b2f3b48440f4dd4f498
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-10 16:40:33 -0300

    improve code

--- linked_list/kth_last.ts
+++ linked_list/kth_last.ts
@@ -5,12 +5,12 @@ import { createLinkedList, LinkedListNode } from "./create_linked_list";
 /**
  * Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
  */
-function printKthToLast(node: LinkedListNode, k: number) {
+function getKthToLastRecursive(node: LinkedListNode, k: number) {
   if (node === undefined) {
     return { index: 0 };
   }
 
-  let { index, value } = printKthToLast(node.next!, k);
+  let { index, value } = getKthToLastRecursive(node.next!, k);
 
   index += 1;
 
@@ -22,11 +22,32 @@ function printKthToLast(node: LinkedListNode, k: number) {
   return { index, value };
 }
 
+function getKthToLast(node: LinkedListNode, k: number) {
+  let current: LinkedListNode | undefined = node;
+  let next: LinkedListNode | undefined = node;
+
+  for (let i = 0; i < k; i++) {
+    if (next === undefined) {
+      return null;
+    }
+    next = next.next;
+  }
+
+  while (next !== undefined) {
+    current = current!.next;
+    next = next.next;
+  }
+
+  return current;
+}
+
 test("return kth to last", () => {
   const linked_list = createLinkedList<number>([1, 2, 4, 5, 6, 7, 2, 1]);
 
-  const { value } = printKthToLast(linked_list, 3);
+  const { value } = getKthToLastRecursive(linked_list, 3);
+  const node = getKthToLast(linked_list, 3);
 
   assert.equal(value, 7);
+  assert.equal(node!.value, 7);
 });
 
