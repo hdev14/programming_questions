920c99d92df8273e71a790abc541f12017e6916b
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-11 17:20:38 -0300

    wip

---
+++ stacks_and_queues/three_in_one.ts
@@ -0,0 +1,13 @@
+import assert from "node:assert";
+import test from "node:test";
+
+/**
+ * Three in One: Describe how you could use a single array to implement three stacks.
+ */
+
+function wip() {
+}
+
+test("three in one", () => {
+  assert(true);
+});
