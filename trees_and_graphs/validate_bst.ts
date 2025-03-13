69e0515ef89ea968b50f85012fea0376b708b326
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-13 17:20:06 -0300

    wip

---
+++ trees_and_graphs/validate_bst.ts
@@ -0,0 +1,10 @@
+import assert from "node:assert";
+import test from "node:test";
+
+/**
+ * Validate BST: Implement a function to check if a binary tree is a binary search tree.
+ */
+
+test("validate binary searh tree", () => {
+  assert(true);
+});
