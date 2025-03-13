80874dce1c876d05db6b64902c1ef4b7e2614dcf
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-12 21:11:06 -0300

    animal shelter

--- stacks_and_queues/animal_shelter.ts
+++ stacks_and_queues/animal_shelter.ts
@@ -10,7 +10,83 @@ import test from "node:test";
  * and dequeueCat.
  */
 
+class Animal {
+  public order: number;
+  constructor(
+    readonly name: string,
+  ) { }
+
+  isOtherThan(animal: Animal) {
+    return this.order > animal.order;
+  }
+}
+
+class Dog extends Animal { }
+
+class Cat extends Animal { }
+
+
+class Shelter {
+  #dogs: Dog[] = [];
+  #cats: Cat[] = [];
+  #order = 0;
+
+  enqueue(animal: Animal) {
+    animal.order = this.#order;
+    this.#order++;
+
+    if (animal instanceof Dog) {
+      this.#dogs.push(animal);
+    }
+
+    if (animal instanceof Cat) {
+      this.#cats.push(animal);
+    }
+  }
+
+  dequeueAny() {
+    if (this.#cats.length === 0) {
+      return this.dequeueDog();
+    }
+
+    if (this.#dogs.length === 0) {
+      return this.dequeueCat();
+    }
+
+    const cat = this.#cats[this.#cats.length - 1];
+    const dog = this.#dogs[this.#dogs.length - 1];
+
+    if (cat.isOtherThan(dog)) {
+      return this.dequeueCat();
+    }
+
+    return this.dequeueDog;
+  }
+
+  dequeueDog() {
+    return this.#dogs.pop();
+  }
+
+  dequeueCat() {
+    return this.#cats.pop();
+  }
+}
+
 
 test("animal shelter", () => {
-  assert(true);
+  const shelter = new Shelter();
+  const cat1 = new Cat('kitty');
+  const dog1 = new Dog('little black');
+  const dog2 = new Dog('mylon');
+  const cat2 = new Cat('stripped');
+
+  shelter.enqueue(cat1);
+  shelter.enqueue(dog1);
+  shelter.enqueue(dog2);
+  shelter.enqueue(cat2);
+
+  assert.equal(shelter.dequeueDog(), dog2);
+  assert.equal(shelter.dequeueAny(), cat2);
+  assert.equal(shelter.dequeueCat(), cat1);
+  assert.equal(shelter.dequeueAny(), dog1);
 });
