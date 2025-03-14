7f201924dbd31268013db4d0cc607f08dbf6dab3
Hermerson Araújo <ne.hermerson@gmail.com>
2025-03-14 15:04:15 -0300

    improve code

--- trees_and_graphs/validate_bst.ts
+++ trees_and_graphs/validate_bst.ts
@@ -6,20 +6,43 @@ import TreeNode from './tree';
  * Validate BST: Implement a function to check if a binary tree is a binary search tree.
  */
 
-function extractNodes(node: TreeNode | null, array: Array<any>, index: number) {
+let index = 0;
+function extractNodes(node: TreeNode | null, array: Array<any>) {
   if (node === null) {
     return;
   }
-  extractNodes(node.left, array, index);
+  extractNodes(node.left, array);
   array[index++] = node.value;
-  extractNodes(node.right, array, index);
+  extractNodes(node.right, array);
+}
+
+let last_element: any = null;
+function checkBST(node: TreeNode | null) {
+  if (node === null) {
+    return true;
+  }
+
+  if (!checkBST(node.left)) {
+    return false;
+  }
+
+  if (last_element !== null && last_element > node.value) {
+    return false;
+  }
+
+  last_element = node.value;
+
+  if (!checkBST(node.right)) {
+    return false;
+  }
+
+  return true;
 }
 
 function isBST(root: TreeNode) {
-  let index = 0;
   let array = [];
-  extractNodes(root, array, index);
-
+  extractNodes(root, array);
+  index = 0;
   for (let idx = 1; idx < array.length; idx++) {
     if (array[idx] <= array[idx - 1]) {
       return false;
@@ -27,6 +50,12 @@ function isBST(root: TreeNode) {
   }
 
   return true;
+}
+
+function isBST2(root: TreeNode) {
+  const result = checkBST(root);
+  last_element = null;
+  return result;
 }
 
 test("validate binary searh tree", () => {
@@ -47,17 +76,19 @@ test("validate binary searh tree", () => {
   const bst = new TreeNode(
     4,
     new TreeNode(
-      3,
+      2,
       new TreeNode(1),
-      new TreeNode(2)
+      new TreeNode(3)
     ),
     new TreeNode(
-      5,
-      new TreeNode(6),
+      6,
+      new TreeNode(5),
       new TreeNode(7)
     )
   );
 
   assert.equal(isBST(not_bst), false);
   assert.equal(isBST(bst), true);
+  assert.equal(isBST2(not_bst), false);
+  assert.equal(isBST2(bst), true);
 });
